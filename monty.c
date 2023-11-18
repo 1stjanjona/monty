43be397739ab6eec91887d4c24758044e5efd41c
1stjanjona <jannat.hammouche@outlook.com>
2023-11-18 12:14:53 +0100

    gejtd

--- monty.c
+++ monty.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+#include <string.h>
+#include <stdio.h>
 void (*get_op_code(char *op))(stack_t **, unsigned int);
 int execute_monty(FILE *f);
 int check_line(char *line, char *dlmtrs);
@@ -12,7 +14,7 @@ void (*get_op_code(char *op))(stack_t **, unsigned int)
 	size_t ndx;
 
 	instruction_t code_op[] = {
-		{"push", opcode_push}, {"pall", opcode_pall}, {"pint", opcode_pint}, {"pop", opcode_pop}, {"swap", opcode_swap}, {"add", opcode_add}, {"nop", opcode_nop}, {"div", opcode_div}, {"pchar", opcode_pchar}, {"pstr", opcode_pstr}, {"stack", opcode_stack}, {"queue", opcode_queue}, {"sub", opcode_sub}, {"mul", opcode_mul}, {"rotr", opcode_rotr}, {"rotl", opdoce_rotl}, {"mod", opcode_mod}, {NULL, NULL}};
+		{"push", handle_push}, {"pall", handle_pall}, {"pint", handle_pint}, {"pop", handle_pop}, {"swap", handle_swap}, {"add", opcode_add}, {"nop", opcode_nop}, {"div", opcode_div}, {"pchar", opcode_pchar}, {"pstr", opcode_pstr}, {"stack", opcode_stack}, {"queue", opcode_queue}, {"sub", opcode_sub}, {"mul", opcode_mul}, {"rotr", opcode_rotr}, {"rotl", opdoce_rotl}, {"mod", opcode_mod}, {NULL, NULL}};
 	for (ndx = 0; code_op[ndx].op; ndx++)
 	{
 		if (strcmp(op, code_op[ndx].op) == 0)
